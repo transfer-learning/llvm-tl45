1bbe24eb1195b3496ad376304540b44dcda8f346
codetector <codetector@codetector.cn>
2020-03-07 18:35:17 -0500

    [Backend] TL45: support for brind

--- llvm/lib/Target/TL45/TL45.h
+++ llvm/lib/Target/TL45/TL45.h
@@ -4,6 +4,8 @@
 
 #ifndef TARGET_TL45_H
 #define TARGET_TL45_H
+
+#include <llvm/CodeGen/AsmPrinter.h>
 #include "MCTargetDesc/TL45MCTargetDesc.h"
 #include "llvm/Target/TargetMachine.h"
 
@@ -12,6 +14,9 @@ namespace llvm {
     class TL45TargetMachine;
     class MCInst;
     FunctionPass *createTL45ISelDag(TL45TargetMachine &TM, CodeGenOpt::Level OptLevel);
+    bool LowerTL45MachineOperandToMCOperand(const MachineOperand &MO,
+                                               MCOperand &MCOp,
+                                               const AsmPrinter &AP);
 } // end namespace llvm;
 
 #endif
--- llvm/lib/Target/TL45/TL45AsmPrinter.cpp
+++ llvm/lib/Target/TL45/TL45AsmPrinter.cpp
@@ -26,61 +26,69 @@ public:
   bool PrintAsmOperand(const MachineInstr *MI, unsigned OpNo,
                                        const char *ExtraCode, raw_ostream &OS) override;
 
+  bool lowerOperand(const MachineOperand &MO, MCOperand &MCOp) const {
+    return LowerTL45MachineOperandToMCOperand(MO, MCOp, *this);
+  }
+
 };
 }
+
 
 static MCOperand lowerSymbolOperand(const MachineOperand &MO, MCSymbol *Sym,
                                     const AsmPrinter &AP) {
   MCContext &Ctx = AP.OutContext;
 
   const MCExpr *ME =
-          MCSymbolRefExpr::create(Sym, MCSymbolRefExpr::VK_None, Ctx);
+      MCSymbolRefExpr::create(Sym, MCSymbolRefExpr::VK_None, Ctx);
 
   if (!MO.isJTI() && !MO.isMBB() && MO.getOffset())
     ME = MCBinaryExpr::createAdd(
-            ME, MCConstantExpr::create(MO.getOffset(), Ctx), Ctx);
+        ME, MCConstantExpr::create(MO.getOffset(), Ctx), Ctx);
 
   return MCOperand::createExpr(ME);
 }
-
-static bool LowerTL45MachineOperandToMCOperand(const MachineOperand &MO,
-                                               MCOperand &MCOp,
-                                               const AsmPrinter &AP) {
+namespace llvm {
+bool LowerTL45MachineOperandToMCOperand(const MachineOperand &MO,
+                                        MCOperand &MCOp,
+                                        const AsmPrinter &AP) {
   switch (MO.getType()) {
-    default:
-      report_fatal_error("LowerTL45MachineInstrToMCInst: unknown operand type");
-    case MachineOperand::MO_Register:
-      // Ignore all implicit register operands.
-      if (MO.isImplicit())
-        return false;
-      MCOp = MCOperand::createReg(MO.getReg());
-      break;
-    case MachineOperand::MO_RegisterMask:
-      // Regmasks are like implicit defs.
+  default:
+    report_fatal_error("LowerTL45MachineInstrToMCInst: unknown operand type");
+  case MachineOperand::MO_Register:
+    // Ignore all implicit register operands.
+    if (MO.isImplicit())
       return false;
-    case MachineOperand::MO_Immediate:
-      MCOp = MCOperand::createImm(MO.getImm());
-      break;
-    case MachineOperand::MO_MachineBasicBlock:
-      MCOp = lowerSymbolOperand(MO, MO.getMBB()->getSymbol(), AP);
-      break;
-    case MachineOperand::MO_GlobalAddress:
-      MCOp = lowerSymbolOperand(MO, AP.getSymbol(MO.getGlobal()), AP);
-      break;
-    case MachineOperand::MO_BlockAddress:
-      MCOp = lowerSymbolOperand(
-              MO, AP.GetBlockAddressSymbol(MO.getBlockAddress()), AP);
-      break;
-    case MachineOperand::MO_ExternalSymbol:
-      MCOp = lowerSymbolOperand(
-              MO, AP.GetExternalSymbolSymbol(MO.getSymbolName()), AP);
-      break;
-    case MachineOperand::MO_ConstantPoolIndex:
-      MCOp = lowerSymbolOperand(MO, AP.GetCPISymbol(MO.getIndex()), AP);
-      break;
+    MCOp = MCOperand::createReg(MO.getReg());
+    break;
+  case MachineOperand::MO_RegisterMask:
+    // Regmasks are like implicit defs.
+    return false;
+  case MachineOperand::MO_Immediate:
+    MCOp = MCOperand::createImm(MO.getImm());
+    break;
+  case MachineOperand::MO_MachineBasicBlock:
+    MCOp = lowerSymbolOperand(MO, MO.getMBB()->getSymbol(), AP);
+    break;
+  case MachineOperand::MO_GlobalAddress:
+    MCOp = lowerSymbolOperand(MO, AP.getSymbol(MO.getGlobal()), AP);
+    break;
+  case MachineOperand::MO_BlockAddress:
+    MCOp = lowerSymbolOperand(
+        MO, AP.GetBlockAddressSymbol(MO.getBlockAddress()), AP);
+    break;
+  case MachineOperand::MO_ExternalSymbol:
+    MCOp = lowerSymbolOperand(
+        MO, AP.GetExternalSymbolSymbol(MO.getSymbolName()), AP);
+    break;
+  case MachineOperand::MO_ConstantPoolIndex:
+    MCOp = lowerSymbolOperand(MO, AP.GetCPISymbol(MO.getIndex()), AP);
+    break;
   }
   return true;
 }
+}
+
+
 
 // adapted from RISCV target.
 bool TL45AsmPrinter::PrintAsmOperand(const MachineInstr *MI, unsigned OpNo,
